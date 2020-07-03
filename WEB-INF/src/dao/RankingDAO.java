ee471ca8baa86bddc90b0a886a1ad7b1aef39bc1
yuki kokado <49096868+kokksyuki@users.noreply.github.com>
2020-07-03 11:03:12 +0900

    rename rbean

--- WEB-INF/src/dao/RankingDAO.java
+++ WEB-INF/src/dao/RankingDAO.java
@@ -13,15 +13,18 @@ public class RankingDAO {
 	public static final String RANKING_ALL_SELECT = "SELECT * FROM RANKING";
 
 	//クエリ内容の追加
-	public static int insert(RankingBean cbean) throws SQLException{
+	//変数名cbean→rbeanに変更2020/07/03
+	public static int insert(RankingBean rbean) throws SQLException{
 		String sql = "INSERT INTO CUSTOMER" +
 					 "(NO,NAME,MONEY) VALUES(" +
-					 "'" + cbean.getRno() + "'," +
-					 "'" + cbean.getRname() + "'," +
-					 cbean.getScore() + ")";
+					 "'" + rbean.getRno() + "'," +
+					 "'" + rbean.getRname() + "'," +
+					 rbean.getScore() + ")";
 		return DBManager.simpleUpdate(sql);
 	}
 
+
+	//sql文でデータベースからクエリ内容を取得
 	public static List<RankingBean> getCustomerList() throws SQLException{
 		String sql = RANKING_ALL_SELECT;
 		return DBManager.findAll(sql, new RankingBeanMapping());
