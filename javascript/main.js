7ce43d73a9d0bdcbfb24b80cf22abac0c484d667
yuki kokado <49096868+kokksyuki@users.noreply.github.com>
2020-07-11 17:04:12 +0900

    add main-view

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,29 @@
-window.onload = function() {
-	text_setCanvas();
-};
+function battle(element){
+
+				drawCircleR(element);
+}
+
 
 
 function text_setCanvas(){
-		var canvas = document.getElementById("canvas");
-		var ctx = canvas.getContext("2d");
-
-
-
-		var startAngle = 0 ;
-		var endAngle =  150;
+	var canvas = document.getElementById("canvas");
+	canvas.width = canvas.offsetWidth;
+	canvas.height = canvas.offsetHeight;
+	var ctx = canvas.getContext("2d");
 		var x = 390;  //左上の頂点x座標
         var y = 530;  //左上の頂点y座標
         var w = 40;  //横の長さ
         var h = 40;  //縦の長さ
-        var r = 10 ;  //角丸の半径
+        var r = 10;  //角丸の半径
         var color = '#767171';  //塗りつぶし色
+
+
 
 		//真ん中
 		ctx.fillStyle='#2F5597';
 		ctx.fillRect(200, 0,400,600);
 
 		//マイク
-
 		//棒
 		ctx.fillStyle= color;
 		ctx.fillRect(400, 550,20,50);
@@ -57,94 +57,263 @@ function text_setCanvas(){
         ctx.stroke();  //←⑦
         ctx.fill();  //←⑧
 
-		//左体
-		ctx.beginPath();
-		ctx.moveTo(200,480); //最初の点の場所
-		ctx.lineTo(150,600); //2番目の点の場所
-		ctx.lineTo(250,600); //3番目の点の場所
-		ctx.closePath();	//三角形の最後の線 closeさせる
-		ctx.fillStyle='#000000';
-		ctx.fill();
+        //相方、プレイヤーの描画
+        player();
+        partner();
+}
 
-		//Yシャツ
-		ctx.beginPath();
-		ctx.moveTo(180,528); //最初の点の場所
-		ctx.lineTo(190,588); //3番目の点の場所
-		ctx.lineTo(210,588); //5番目の点の場所
-		ctx.lineTo(220,528); //2番目の点の場所
-		ctx.closePath();	//三角形の最後の線 closeさせる
-		ctx.fillStyle='#ffffff';
-		ctx.fill();
+//以下プレイヤー作成
+function player(){
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var startAngle = 280 ;
+	var endAngle =  380;
+	var centerx = 600;
+	var centery = 480;
 
+	//右体
+	ctx.beginPath();
+	ctx.moveTo(centerx,centery); //最初の点の場所
+	ctx.lineTo(centerx-50,centery+120); //2番目の点の場所
+	ctx.lineTo(centerx+50,centery+120); //3番目の点の場所
+	ctx.closePath();	//三角形の最後の線 closeさせる
+	ctx.fillStyle='#000000';
+	ctx.fill();
 
+	//Yシャツ
+	ctx.beginPath();
+	ctx.moveTo(580,528); //最初の点の場所
+	ctx.lineTo(590,588); //2番目の点の場所
+	ctx.lineTo(610,588); //3番目の点の場所
+	ctx.lineTo(620,528); //4番目の点の場所
+	ctx.closePath();	//三角形の最後の線 closeさせる
+	ctx.fillStyle='#ffffff';
+	ctx.fill();
 
-
-
-
-		//ネクタイ
-		ctx.beginPath();
-		ctx.moveTo(200,518); //最初の点の場所
-		ctx.lineTo(190,540); //3番目の点の場所
-		ctx.lineTo(190,588); //4番目の点の場所
-		ctx.lineTo(210,588); //5番目の点の場所
-		ctx.lineTo(210,540); //6番目の点の場所
-		ctx.closePath();	//矢印の最後の線 closeさせる
-		ctx.fillStyle='red';
-		ctx.fill();
-
-
-		//左頭
-		ctx.beginPath () ;
-		ctx.arc( 200, 480, 50, (startAngle-60) * Math.PI / 360, (endAngle-60) * Math.PI / 360, true ) ;
-		ctx.lineTo(200, 480);
-		ctx.fillStyle='#FBE5D6';
-		ctx.fill();
-
-
-
-
-		//右体
-		ctx.beginPath();
-		ctx.moveTo(600,480); //最初の点の場所
-		ctx.lineTo(550,600); //2番目の点の場所
-		ctx.lineTo(650,600); //3番目の点の場所
-		ctx.closePath();	//三角形の最後の線 closeさせる
-		ctx.fillStyle='#000000';
-		ctx.fill();
-
-		//Yシャツ
-		ctx.beginPath();
-		ctx.moveTo(580,528); //最初の点の場所
-		ctx.lineTo(590,588); //3番目の点の場所
-		ctx.lineTo(610,588); //5番目の点の場所
-		ctx.lineTo(620,528); //2番目の点の場所
-		ctx.closePath();	//三角形の最後の線 closeさせる
-		ctx.fillStyle='#ffffff';
-		ctx.fill();
-
-		//ネクタイ
-		ctx.beginPath();
-		ctx.moveTo(600,528); //最初の点の場所
-		ctx.lineTo(590,540); //3番目の点の場所
-		ctx.lineTo(590,588); //4番目の点の場所
-		ctx.lineTo(610,588); //5番目の点の場所
-		ctx.lineTo(610,540); //6番目の点の場所
-		ctx.closePath();	//矢印の最後の線 closeさせる
-		ctx.fillStyle='yellow';
-		ctx.fill();
+	//ネクタイ
+	ctx.beginPath();
+	ctx.moveTo(centerx,528); //最初の点の場所
+	ctx.lineTo(590,540); //2番目の点の場所
+	ctx.lineTo(590,588); //3番目の点の場所
+	ctx.lineTo(610,588); //4番目の点の場所
+	ctx.lineTo(610,540); //6番目の点の場所
+	ctx.closePath();	//矢印の最後の線 closeさせる
+	ctx.fillStyle='yellow';
+	ctx.fill();
 
 
-		//右頭
-		ctx.beginPath () ;
-		ctx.arc( 600, 480, 50, (startAngle+280) * Math.PI / 360, (endAngle+280) * Math.PI / 360, true ) ;
-		ctx.lineTo(600, 480);
-		ctx.fillStyle='#FBE5D6';
-		ctx.fill();
-
-
+	//右頭
+	ctx.beginPath () ;
+	ctx.arc( centerx, 480, 50, startAngle * Math.PI / 360, endAngle * Math.PI / 360, true );
+	ctx.lineTo(centerx, 480);
+	ctx.fillStyle='#FBE5D6';
+	ctx.fill();
 }
 
 
+//以下パートナー 座標は調整中
+function partner(){
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var startAngle = 0 ;
+	var endAngle =  150;
+
+	//左体
+	ctx.beginPath();
+	ctx.moveTo(200,480); //最初の点の場所
+	ctx.lineTo(150,600); //2番目の点の場所
+	ctx.lineTo(250,600); //3番目の点の場所
+	ctx.closePath();	//三角形の最後の線 closeさせる
+	ctx.fillStyle='#000000';
+	ctx.fill();
+
+	//Yシャツ
+	ctx.beginPath();
+	ctx.moveTo(180,528); //最初の点の場所
+	ctx.lineTo(190,588); //3番目の点の場所
+	ctx.lineTo(210,588); //5番目の点の場所
+	ctx.lineTo(220,528); //2番目の点の場所
+	ctx.closePath();	//三角形の最後の線 closeさせる
+	ctx.fillStyle='#ffffff';
+	ctx.fill();
+
+	//ネクタイ
+	ctx.beginPath();
+	ctx.moveTo(200,518); //最初の点の場所
+	ctx.lineTo(190,540); //3番目の点の場所
+	ctx.lineTo(190,588); //4番目の点の場所
+	ctx.lineTo(210,588); //5番目の点の場所
+	ctx.lineTo(210,540); //6番目の点の場所
+	ctx.closePath();	//矢印の最後の線 closeさせる
+	ctx.fillStyle='red';
+	ctx.fill();
+
+
+	//左頭
+	ctx.beginPath () ;
+	ctx.arc( 200, 480, 50, (startAngle-60) * Math.PI / 360, (endAngle-60) * Math.PI / 360, true ) ;
+	ctx.lineTo(200, 480);
+	ctx.fillStyle='#FBE5D6';
+	ctx.fill();
+}
+
+
+//以下吹き出し
+function drawCircleL(bbean){
+
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var size = 15;		//文字サイズ
+    var padding = 5;	//余白
+    var w1 = 250;		//コメントの横の長さ
+    var h1 = size;		//コメントの縦の長さ
+    var r1 = 10;		//四角の角丸の半径
+    var text = bbean; 	//テキスト
+    var x1 = 280;		//左上の頂点x座標
+    var y1 = 320-h1;		//左上の頂点y座標
+
+    //吹き出し作成
+	function boke(){
+		ctx.beginPath();
+		ctx.lineWidth = 1;
+		ctx.fillStyle='white';
+		ctx.strokeStyle = 'black';
+		ctx.moveTo(x1,y1 + r1);
+		ctx.arc(x1+r1,y1+h1-r1,r1,Math.PI,Math.PI*0.5,true);
+		ctx.lineTo(x1,y1+h1+r1*2);
+		ctx.lineTo(x1+r1+30,y1+h1);
+		ctx.arc(x1+w1-r1,y1+h1-r1,r1,Math.PI*0.5,0,1);
+		ctx.arc(x1+w1-r1,y1+r1,r1,0,Math.PI*1.5,1);
+		ctx.arc(x1+r1,y1+r1,r1,Math.PI*1.5,Math.PI,1);
+		ctx.closePath();
+		ctx.stroke();
+		ctx.fill();
+	}
+
+	//吹き出し呼び出し
+	boke();
+
+
+	//テキスト設定
+    var limitedWidth =w1-(padding * 2);
+	var limitedHeight = h1-(padding * 2);
+	ctx.font = size + "px ''";
+
+	//テキスト調整　行に分解
+	var lineTextList = text.split("\n");
+	var newLineTextList = [];
+	lineTextList.forEach(function (lineText) {
+	  if (ctx.measureText(lineText).width > limitedWidth) {
+	      characterList = lineText.split("");// 1文字ずつ分割
+	      var preLineText = "";
+	      var lineText = "";
+	      characterList.forEach(function (character) {
+	          lineText += character;
+	          if (ctx.measureText(lineText).width > limitedWidth) {
+	              newLineTextList.push(preLineText);
+	              lineText = character;
+	              //縦枠のリサイズ
+	              if(limitedHeight < size){
+		        	  h1 += (size*1.6);
+		        	  boke();
+		          }
+	          }
+	          //改行
+	          preLineText = lineText;
+
+	      });
+	  }
+	  newLineTextList.push(lineText);
+	});
+
+	var lineLength = newLineTextList.length;
+
+
+	//テキストの描画
+	ctx.fillStyle = "#000000";
+	newLineTextList.forEach(function (lineText, index) {
+	  ctx.fillText(lineText,x1+padding,y1+padding+(size * (index + 1)));
+	});
+
+
+	ctx.save();
+	}
+
+function drawCircleR(abean){
+
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+
+	var x1 = 280;  			//左上の頂点x座標
+    var y1 = 420;  			//左上の頂点y座標
+    var size = 15;			//文字サイズ
+    var w1 = 250; 			//吹き出しの横の長さ
+    var h1 = (size+20);  	//吹き出しの縦の長さ
+    var padding = 5;		//余白
+    var r1 = 10;  			//四角の角丸の半径
+    var text=abean;			//テキスト
+    var x1 = 280;  			//左上の頂点x座標
+    var y1 = 420;  			//左上の頂点y座標
+
+
+    //吹き出し作成
+    function answer(){
+		ctx.beginPath();
+		ctx.lineWidth = 1;
+		ctx.fillStyle='white';
+		ctx.strokeStyle = 'black';
+		ctx.moveTo(x1,y1 + r1);
+		ctx.arc(x1+r1,y1+h1-r1,r1,Math.PI,Math.PI*0.5,true);
+		ctx.lineTo(x1+w1-30,y1+h1);
+		ctx.lineTo(x1+w1,y1+h1+r1*2);
+		ctx.arc(x1+w1-r1,y1+h1-r1,r1,Math.PI*0.5,0,1);
+		ctx.arc(x1+w1-r1,y1+r1,r1,0,Math.PI*1.5,1);
+		ctx.arc(x1+r1,y1+r1,r1,Math.PI*1.5,Math.PI,1);
+		ctx.closePath();
+		ctx.stroke();
+		ctx.fill();
+    }
+
+    answer();
+
+	//テキスト設定
+	var limitedWidth =w1-(padding * 2);
+	var limitedHeight = h1-(padding * 2);
+	ctx.font = size + "px ''";
+
+	// テキスト調整　行に分解
+	var lineTextList = text.split("\n");
+	var newLineTextList = [];
+	lineTextList.forEach(function (lineText) {
+	    if (ctx.measureText(lineText).width > limitedWidth) {
+	        characterList = lineText.split("");// 1文字ずつ分割
+	        var preLineText = "";
+	        var lineText = "";
+	        characterList.forEach(function (character) {
+	            lineText += character;
+	            if (ctx.measureText(lineText).width > limitedWidth) {
+	                newLineTextList.push(preLineText);
+	                lineText = character;
+	                if(limitedHeight < size){
+			        	  h1 += (size*1.6);
+			        	  answer();
+			          }
+	            }
+	            preLineText = lineText;
+	        });
+	    }
+	    newLineTextList.push(lineText);
+	});
+
+	var lineLength = newLineTextList.length;
+
+	ctx.fillStyle = "#000000";
+	newLineTextList.forEach(function (lineText, index) {
+	    ctx.fillText(lineText,x1+padding,y1+padding+(size * (index + 1)));
+	});
+
+	ctx.save();
+}
 
 
 
--- view/main.jsp
+++ view/main.jsp
@@ -21,7 +21,10 @@
 
 		<!-- CSS読み込み開始 -->
 			<script type="text/javascript">common_link();</script>
+			<link rel="stylesheet" href="css/main.css">
 		<!-- CSS読み込み終了 -->
+
+
 	</head>
 
 	<body>
@@ -33,13 +36,14 @@
 
 		<!-- メイン画面開始 -->
 		<!-- 20200703 加納 centerタグ変更-->
-		<div style="text-align: center">
+<!--  -->
+		<div style="text-align: center"  class ="parent">
 			<!-- canvas開始 -->
 				<script type="text/javascript">canvas_func();</script>
 				<script type="text/javascript">text_setCanvas();</script>
-
-
 			<!-- canvas終了 -->
+
+
 
 
 			<font color="white">
@@ -53,8 +57,8 @@
 			%>
 
 			<br>
-<% RegistNameBean registName = (RegistNameBean)session.getAttribute("rname");
-		PointBean point = (PointBean)session.getAttribute("point");%>
+			<% RegistNameBean registName = (RegistNameBean)session.getAttribute("rname");
+				PointBean point = (PointBean)session.getAttribute("point");%>
 			<tr>
 				<th><%= pbean.getPid() %></th>
 				<th><%= pbean.getPname() %></th>
@@ -63,6 +67,7 @@
 				<th><%= registName.getName() %></th>
 
 			</tr>
+
 	 		<br>
 	 		<%
 				}
@@ -72,6 +77,8 @@
 					for(int i = 0; i < bokeList.size(); i++){
 					BokeBean bbean = bokeList.get(i);
 			%>
+
+			<script type="text/javascript">drawCircleL("<%=bbean.getBcontext()%>");</script>
 	 		<tr>
 				<th><%=bbean.getBid()%></th>
 				<th><%=bbean.getBcontext()%></th>
@@ -83,26 +90,64 @@
 				}
 			%>
 
+
 			<%
-				List<AnsBean> ansList = (List<AnsBean>)session.getAttribute("ansList");
-					for(int i = 0; i < ansList.size(); i++){
-					AnsBean abean = ansList.get(i);
+				List<AnsBean> AnsList = (List<AnsBean>)session.getAttribute("ansList");
+				for(int i = 0; i < AnsList.size(); i++){
+					AnsBean abean = AnsList.get(i);
 			%>
-			<tr>
+					<script type="text/javascript">drawCircleR("<%=abean.getSanswer()%>");</script>
+
+
+
+
 				<th><%=abean.getAid()%></th>
 
 				<th><%=abean.getAscore()%></th>
 			<!-- (2020/7/7)古門　ツッコミをラジオボタンで選択できるようにしてます 以下変更-->
-						<form action="<%= request.getContextPath() %>/main"method="Post">
-							<input type="radio" name="Answer" value="A" class="answer-check">1：
-								<th><%=abean.getSanswer()%></th><br>
-							<input type="radio" name="Answer" value="B" class="answer-check">2：
-								<th><%=abean.getNanswer()%></th><br>
-							<input type="radio" name="Answer" value="C" class="answer-check"  required>3：
-								<th><%=abean.getTanswer()%></th><br>
+
+					<div class ="child" style="text-align:left;">
+
+
+						<form action=" <%= request.getContextPath() %>/main" method="Post" id="answer-check">
+							<input type="radio" name="Answer" value="A" class="answer-check">
+							1：
+								<%=abean.getSanswer()%><br>
+							<input type="radio" name="Answer" value="B" class="answer-check">
+							2：
+								<%=abean.getNanswer()%><br>
+							<input type="radio" name="Answer" value="C" class="answer-check" required>
+							3：
+								<%=abean.getTanswer()%><br>
 							<input type="submit"value="突っ込め！！"name="ANSWER">
 						</form>
-			</tr>
+						<script type="text/javascript" id ="tukkomi">
+							var element = document.getElementById("answer-check") ;
+							var radioNodeList = element.Answer;
+							var a = radioNodeList.value;
+							var tukkomi="";
+							switch(a){
+							case A;
+								tukkomi = "<%=abean.getSanswer()%>"
+								break;
+							case B:
+								tukkomi = "<%=abean.getNanswer()%>"
+									break;
+							case C:
+								tukkomi = "<%=abean.getTanswer()%>"
+									break;
+							default:
+								return -1;
+							}
+						</script>
+
+					</div>
+
+				<script type="text/javascript" id ="tukkomi">
+				battle(tukkomi);
+				</script>
+
+
 			<!-- (2020/7/7)古門　ツッコミをラジオボタンで選択できるようにしてます 以上変更-->
 			<%
 				}
